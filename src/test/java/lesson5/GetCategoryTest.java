c6a1dcff1df735f5ba25cf884e46f1018f250801
Avella <akemi.konoe@yandex.ru>
2022-12-19 20:07:27 +0400

    Урок 5. Автоматизированное тестирование REST API c использованием Retrofit/OkHttp3

---
+++ src/test/java/lesson5/GetCategoryTest.java
@@ -0,0 +1,55 @@
+package lesson5;
+
+import lesson5.api.CategoryService;
+import lesson5.dto.GetCategoryResponse;
+import lesson5.utils.RetrofitUtils;
+import lombok.SneakyThrows;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import retrofit2.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetCategoryTest {
+
+    static CategoryService categoryService;
+    @BeforeAll
+    static void beforeAll() {
+        categoryService = RetrofitUtils.getRetrofit().create(CategoryService.class);
+    }
+
+    @SneakyThrows
+    @Test
+    void getCategoryByIdPositiveTest() {
+        Response<GetCategoryResponse> response = categoryService.getCategory(1).execute();
+
+        assertThat(response.isSuccessful(), CoreMatchers.is(true));
+        assertThat(response.body().getId(), equalTo(1));
+        assertThat(response.body().getTitle(), equalTo("Food"));
+        response.body().getProducts().forEach(product ->
+                assertThat(product.getCategoryTitle(), equalTo("Food")));
+    }
+
+    @SneakyThrows
+    @Test
+    void getCategoryByIdTest() {
+        Response<GetCategoryResponse> response = categoryService.getCategory(2).execute();
+
+        assertThat(response.isSuccessful(), CoreMatchers.is(true));
+        assertThat(response.body().getId(), equalTo(2));
+        assertThat(response.body().getTitle(), equalTo("Electronic"));
+        response.body().getProducts().forEach((product ->
+                assertThat(product.getCategoryTitle(), equalTo("Electronic"))));
+    }
+
+    @SneakyThrows
+    @Test
+    void getCategoryNegativeTest() {
+        Response<GetCategoryResponse> response = categoryService.getCategory(3).execute();
+
+        assertThat(response.isSuccessful(), CoreMatchers.is(false));
+        assertThat(response.code(), equalTo(404));
+    }
+}
